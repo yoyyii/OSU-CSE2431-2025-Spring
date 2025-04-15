6a92cbd681cc9d0596a0eefcc43307c877c6cc97
yoyyii <114526999+yoyyii@users.noreply.github.com>
2025-04-14 23:24:52 -0400

    Add files via upload

---
+++ lab1/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char*argv[]) {
+
+	printf("Hello, My name is Yoyi Liao.\n");
+	printf("I am taking SP25-CSE2431 about Operating Systems.\n");
+
+	return 0;
+}
